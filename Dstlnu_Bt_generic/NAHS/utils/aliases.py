c433892efadc9cccd33df036e1294824113ccd2a
Axel Heimeroth <axelheim@naf-belle11.desy.de>
2021-10-22 07:52:37 +0200

    as used for NAHS_100kRun1 data run

--- Dstlnu_Bt_generic/NAHS/utils/aliases.py
+++ Dstlnu_Bt_generic/NAHS/utils/aliases.py
@@ -1,9 +1,32 @@
 def define_aliases_Hc():
     alias_dict ={}
-    for i in range(0,2):
-        for j in range(0,3):
+    for i in range(0,2): # B level
+        alias_dict[f'genUp4S_uniqParID_{i}'] = f'genUpsilon4S(mcDaughter({i}, uniqueParticleIdentifier))'
+
+        for j in range(0,3): # D* l nu level
             alias_dict[f'genUp4S_PDG_{i}_{j}'] = f'genUpsilon4S(mcDaughter({i}, mcDaughter({j},PDG)))'
             alias_dict[f'abs_genUp4S_PDG_{i}_{j}'] = f'abs(genUpsilon4S(mcDaughter({i}, mcDaughter({j},PDG))))'
+            alias_dict[f'genUp4S_uniqParID_{i}_{j}'] = f'genUpsilon4S(mcDaughter({i}, mcDaughter({j},uniqueParticleIdentifier)))'
+            if j == 0:
+                for k in range(0,2): # D0 pi level
+                    alias_dict[f'genUp4S_PDG_{i}_{j}_{k}'] = f'genUpsilon4S(mcDaughter({i}, mcDaughter({j},mcDaughter({k},PDG))))'
+                    alias_dict[f'genUp4S_uniqParID_{i}_{j}_{k}'] = f'genUpsilon4S(mcDaughter({i}, mcDaughter({j},mcDaughter({k},uniqueParticleIdentifier))))'
+                    if k == 0: # D0 daughters
+                        for l in range(0,5): 
+                            alias_dict[f'genUp4S_PDG_{i}_{j}_{k}_{l}'] = f'genUpsilon4S(mcDaughter({i}, mcDaughter({j},mcDaughter({k},mcDaughter({l},PDG)))))'
+                            alias_dict[f'genUp4S_uniqParID_{i}_{j}_{k}_{l}'] = f'genUpsilon4S(mcDaughter({i}, mcDaughter({j},mcDaughter({k},mcDaughter({l},uniqueParticleIdentifier)))))'
+                
+    
+    
+    alias_dict['BeamE'] = 'beamE'
+    alias_dict['BeamPx'] = 'beamPx'
+    alias_dict['BeamPy'] = 'beamPy'
+    alias_dict['BeamPz'] = 'beamPz'
+    alias_dict['BeamcmsE'] = 'useCMSFrame(beamE)'
+    alias_dict['BeamcmsPx'] = 'useCMSFrame(beamPx)'
+    alias_dict['BeamcmsPy'] = 'useCMSFrame(beamPy)'
+    alias_dict['BeamcmsPz'] = 'useCMSFrame(beamPz)'
+
 
     for i in range(1): # dont know yet if that's deep enough, 8 seemed okay at first glance
         tmp_var = "genMotherPDG({})".format(i)
@@ -37,6 +60,12 @@ def define_aliases_FSPs():
     for i in range(10): # dont know yet if that's deep enough, 8 seemed okay at first glance
         tmp_var = "genMotherPDG({})".format(i)
         key = "genMothPDG_{}".format(i)
+        alias_dict[key] = tmp_var
+
+        
+    for i in range(10): 
+        tmp_var = "genMotherID({})".format(i)
+        key = "genMotherID_{}".format(i)
         alias_dict[key] = tmp_var
 
 
